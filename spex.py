72e9429621ee705f330d22773a3846227c072422
Austin Bodzas <austinbodzas@gmail.com>
2016-10-10 21:59:37 -0400

    rebased to a different basic template

--- spex.py
+++ spex.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
-@app.route('/second/')
+    return render_template('/index.html')
+
+
+@app.route('/hab/')
 def second():
-    text= "Projects"
-    return render_template('second.html', text=text)
+    return render_template('hab.html')
