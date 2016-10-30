4f606bb7e4f8d7fc9b7146dc8592f298d48a18e7
venku122 <tarazevits@gmail.com>
2016-10-30 00:53:57 -0400

    Adjusting wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
+import os
 from spex import app as application
 virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
-httpd.serve_forever()
+#httpd.serve_forever()
 #
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
