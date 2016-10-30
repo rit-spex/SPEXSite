1fef0d9ad831cc5f3b3dea1649d55914eb9c8d6b
venku122 <tarazevits@gmail.com>
2016-10-30 00:41:00 -0400

    Adjusting wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python
 import os
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+import sys
+
+sys.path.insert(0, os.path.dirname(__file__) or '.')
+
+PY_DIR = os.path.join(os.environ['OPENSHIFT_HOMEDIR'], "python")
+
+virtenv = PY_DIR + '/virtenv/'
+
+PY_CACHE = os.path.join(virtenv,
+                        'lib',
+                        os.environ['OPENSHIFT_PYTHON_VERSION'],
+                        'site-packages')
+
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(PY_CACHE)
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+
 try:
-    execfile(virtualenv, dict(__file__=virtualenv))
+    exec(open(virtualenv).read(), dict(__file__=virtualenv))
 except IOError:
     pass
+    
+from spex import app as application
 
 #
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
 #
-
-from spex import app as application
