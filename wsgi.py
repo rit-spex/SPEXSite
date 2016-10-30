838236b3b5aa0f07f7355777c152b56a3221e433
venku122 <tarazevits@gmail.com>
2016-10-30 01:05:02 -0400

    Adjusting wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
+
 import os
+
+virtenv = os.environ['APPDIR'] + '/virtenv/'
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.6/site-packages')
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
 from spex import app as application
-virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
-#httpd.serve_forever()
-#
-# IMPORTANT: Put any additional includes below this line.  If placed above this
-# line, it's possible required libraries won't be in your searchable path
-#
