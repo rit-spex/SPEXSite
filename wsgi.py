0ef32d716c77e434d121bdfa767880f335c1f3ef
venku122 <tarazevits@gmail.com>
2016-10-30 00:18:06 -0400

    Adjusting wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,12 @@
-
 #!/usr/bin/python
 import os
-
 virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
     execfile(virtualenv, dict(__file__=virtualenv))
 except IOError:
     pass
+
 #
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
