a0a4d454fa8c445125b9af0ba8eb4366575ea55a
venku122 <tarazevits@gmail.com>
2016-10-29 23:13:28 -0400

    Added wsgi.py file to aid in Openshift Deployment

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import os
+
+if 'OPENSHIFT_APP_NAME' in os.environ:              #are we on OPENSHIFT?
+    ip = os.environ['OPENSHIFT_PYTHON_IP']
+    port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
+else:
+    ip = '0.0.0.0'                            #localhost
+    port = 8051
+
+httpd = make_server(ip, port, application)
